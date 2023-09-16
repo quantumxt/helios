45ec4eca493c442e61b7cecec6222cd8b9a53ecf
Photon <1472411+quantumxt@users.noreply.github.com>
2023-09-16 14:07:30 +0800

    fix: Use env var

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 mkdir ~/bin
 cp tools/normcheck.sh ~/bin/normcheck
 cp tools/get_cfiles.sh ~/bin/get_cfiles
-echo "export PATH=$PATH:/home/username/bin" >> ~/.bashrc
+echo "export PATH=$PATH:$HOME/bin" >> ~/.bashrc
 . ~/.bashrc
-# echo "export PATH=$PATH:/home/username/bin" >> ~/.zshrc
+# echo "export PATH=$PATH:$HOME/bin" >> ~/.zshrc
 # . ~/.zshrc
